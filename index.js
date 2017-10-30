d2f2eb94079f6cd306a74fb7d68145dd359b7dc8
Robert Meis <robert.meis@colorado.edu>
2017-10-30 20:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var newArray = [];
 for (var i = 0; i <musicians.length; i++) {
  var music = musicians[i];
  var inst = instruments[i];
-  newArray.push(music + " plays " + inst")
+  newArray.push(music + " plays " + inst)
 }
 
 return newArray;
