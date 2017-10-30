d673ae979ff6faa9a49cd61c432c2d91e5f5bb09
Robert Meis <robert.meis@colorado.edu>
2017-10-30 20:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function theBeatlesPlay(musicians, instruments) {
 var newArray = [];
 
 for (var i = 0; i <musicians.length; i++) {
-
-  newArray.push("${musicians[i]} plays ${insturments[i]}")
+ var music = musicians[i];
+ var inst = instruments[i];
+  newArray.push(music + " plays " + inst")
 }
 
 return newArray;
