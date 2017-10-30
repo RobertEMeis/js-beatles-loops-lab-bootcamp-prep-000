7bd3ca9d04d2e73dcee302610563664864bf9ba7
Robert Meis <robert.meis@colorado.edu>
2017-10-30 20:22:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+function theBeatlesPlay(musicians, instruments) {
+
+var newArray = [];
+
+for (var i = 0; i <musicians.length; i++) {
+
+  newArray.push("${musicians[i]} plays ${insturments[i]}")
+}
+
+return newArray;  
+}
